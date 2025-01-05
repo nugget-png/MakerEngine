7bfc7c24efb96eac08f21faa219704c2bd7d85ec
nugget-png <nugget-png@users.noreply.github.com>
2025-01-05 02:16:17 -0500

    Put currentPlatform function inside of MakerEngine namespace

--- MakerEngine/Global/MakerEngine.cpp
+++ MakerEngine/Global/MakerEngine.cpp
@@ -1,17 +1,19 @@
 #include "MakerEngine.h"
 
-std::string& MakerEngine::currentPlatform() {
-	// Static variable to retain its value across function calls
-	static std::string platform;
+namespace MakerEngine {
+	std::string& currentPlatform() {
+		// Static variable to retain its value across function calls
+		static std::string platform;
 
-	#ifdef PLATFORM_WINDOWS
-		platform = "Windows";
-	#elif PLATFORM_LINUX
-		platform = "Linux";
-	#elif PLATFORM_MACOS
-		platform = "macOS";
-	#else
-		// In case of an error, return "Unknown"
-		platform = "Unknown";
-	#endif
+		#ifdef PLATFORM_WINDOWS
+			platform = "Windows";
+		#elif PLATFORM_LINUX
+			platform = "Linux";
+		#elif PLATFORM_MACOS
+			platform = "macOS";
+		#else
+			// In case of an error, return "Unknown"
+			platform = "Unknown";
+		#endif
+	}
 }
