de56e34b16c84be0697f373b86a4339341e79de9
nugget-png <nugget-png@users.noreply.github.com>
2025-01-06 15:54:30 -0500

    Test window creation

--- GamePlayer/MakerGame.cpp
+++ GamePlayer/MakerGame.cpp
@@ -1,15 +1,20 @@
-﻿#include <iostream>
+#include <iostream>
 #include <MakerEngine.h>
+#include <Application/Window.h>
 
 using namespace MakerEngine;
 
 int main() {
 	Engine engine;
+    Application::Window window;
+
 	engine.initialize();
 
-	spdlog::info("Ready");
-	system("pause");
+    window.create(1280, 720, "Game Player");
+    window.runLoop();
+    window.destroy();
 
 	engine.shutdown();
+
 	return 0;
 }
