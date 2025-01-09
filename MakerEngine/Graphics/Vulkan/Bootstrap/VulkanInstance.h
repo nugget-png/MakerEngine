5c33330c129d5a1a761026efc39e17656293483c
nugget-png <nugget-png@users.noreply.github.com>
2025-01-08 19:53:14 -0500

    Move bootstrap related code into Bootstrap directory

---
+++ MakerEngine/Graphics/Vulkan/Bootstrap/VulkanInstance.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "MakerEngine.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <string>
+#include <stdexcept>
+#include <cstring>
+
+namespace MakerEngine {
+    namespace Graphics {
+        namespace Vulkan {
+            namespace Bootstrap {
+                class MAKERENGINE_API VulkanInstance {
+                public:
+                    VulkanInstance();
+                    ~VulkanInstance();
+
+                    void create(const std::string& appName, uint32_t appVersion);
+
+                    const VkInstance& getVkInstance() const;
+
+                private:
+                    bool checkValidationLayerSupport();
+
+                    std::vector<const char*> getRequiredExtensions();
+
+                    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+                        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                        VkDebugUtilsMessageTypeFlagsEXT messageType,
+                        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                        void* pUserData
+                    );
+
+                    VkResult CreateDebugUtilsMessengerEXT(
+                        VkInstance instance,
+                        const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+                        const VkAllocationCallbacks* pAllocator,
+                        VkDebugUtilsMessengerEXT* pDebugMessenger
+                    );
+
+                    void DestroyDebugUtilsMessengerEXT(
+                        VkInstance instance,
+                        VkDebugUtilsMessengerEXT debugMessenger,
+                        const VkAllocationCallbacks* pAllocator
+                    );
+
+                    void createDebugMessenger();
+
+                    void destroy();
+
+                    bool enableValidationLayers;
+                    VkDebugUtilsMessengerEXT debugMessenger;
+                    VkInstance instance;
+                };
+            }
+        }
+    }
+}
