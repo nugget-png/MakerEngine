1b5f3532a6fe7f3d13fc300d84b2bc4a9206c8af
nugget-png <nugget-png@users.noreply.github.com>
2025-01-13 06:43:58 -0500

    Test logical device creation in MakerGame

--- GamePlayer/MakerGame.cpp
+++ GamePlayer/MakerGame.cpp
@@ -3,6 +3,7 @@
 #include <Application/Window.h>
 #include <Graphics/Vulkan/Bootstrap/VulkanInstance.h>
 #include <Graphics/Vulkan/Bootstrap/PhysicalDevice.h>
+#include <Graphics/Vulkan/Bootstrap/LogicalDevice.h>
 
 using namespace MakerEngine;
 using namespace MakerEngine::Graphics;
@@ -12,18 +13,18 @@ int main() {
     Application::Window window;
     Vulkan::Bootstrap::VulkanInstance vulkanInstance;
     Vulkan::Bootstrap::PhysicalDevice physicalDevice;
+    Vulkan::Bootstrap::LogicalDevice logicalDevice;
 
     engine.initialize();
     vulkanInstance.create("Game Player", VK_MAKE_VERSION(1, 0, 0));
     physicalDevice.pickBestDevice(vulkanInstance);
+    logicalDevice.create(physicalDevice);
  
     window.create(1280, 720, "Game Player");
     window.runLoop();
 
     window.destroy();
     engine.shutdown();
-
-    // No need to clean up VulkanInstance, its done automatically in its destructor
 
     return 0;
 }
